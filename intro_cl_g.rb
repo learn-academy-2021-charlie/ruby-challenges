8abf69f6d60f363210d840b0e7b4a86ee89ff87d
Chrisluna10 <chrisluna4224@gmail.com>
2021-07-28 11:16:44 -0700

    string and arrays challenge completed

--- intro_cl_g.rb
+++ intro_cl_g.rb
@@ -1,7 +1,18 @@
-my_var = 'hello'
-p "#{my_var}, there!"
+# my_var = 'hello'
+# p "#{my_var}, there!"
 
-p my_var.upcase
-p my_var.reverse
-p my_var.include? 'e'
-p my_var.capitalize
+# p my_var.upcase
+# p my_var.reverse
+# p my_var.include? 'e'
+# p my_var.capitalize
+
+
+my_array = ["Friends", "The Office", "That's 70's Show", "New Girl", "Everyone Hates Chris"]
+# p my_array.length
+# p my_array[0]
+# p my_array[3]
+# p my_array.reverse!
+top_fav_shows = []
+top_fav_shows << my_array[0] 
+top_fav_shows << my_array[2]
+p top_fav_shows
