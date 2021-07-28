41007355ec2864178f41b35ff613d5754a1f6ce6
Andy19936 <andy19936@gmail.com>
2021-07-28 11:06:32 -0700

    initial commit

---
+++ intro-andy-alfonso.rb
@@ -0,0 +1,24 @@
+my_favorite_num = 22
+
+p my_favorite_num/2
+
+someones_favorite = 13
+
+p someones_favorite = 7
+p someones_favorite
+p someones_favorite-my_favorite_num
+p my_favorite_num = my_favorite_num*26
+p my_favorite_num
+
+
+
+my_string = 'hello world'
+p my_string
+p "hi #{my_string}"
+
+
+my_string_capitalized = my_string.capitalize 
+p my_string_capitalized
+
+my_string_reversed = my_string.reverse
+p my_string_reversed
