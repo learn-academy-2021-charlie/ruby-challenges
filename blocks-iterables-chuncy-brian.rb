c541ae77fd73e14bbe579aafd70d8d23a8e45f53
Bpompa95 <brianpompa02@gmail.com>
2021-07-28 16:25:31 -0700

    complted 3 questions

---
+++ blocks-iterables-chuncy-brian.rb
@@ -0,0 +1,27 @@
+#Write a loop that prints the numbers 1 to 20.
+
+# num = 1
+# while num <= 20
+#     p num
+#     num += 1
+# end
+
+#Write a loop that prints the numbers 1 to 20 using a different approach than previously used.
+
+# num = 1
+# nums_array = []
+# while num <= 20
+#     nums_array << num 
+# end
+# p nums_array
+
+# Write a loop that prints only even numbers from 20 to 0.
+# def only_evens iterable
+#     iterable.select do [value]
+#         value.even?
+#     end
+# end
+# p only_evens 1..20
+
+
+
