8aab148d715c02183559b943e36ca66ad1d44fe6
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-07-28 11:18:02 -0700

    did array stuff

--- intro-andy-alfonso.rb
+++ intro-andy-alfonso.rb
@@ -22,3 +22,35 @@ p my_string_capitalized
 
 my_string_reversed = my_string.reverse
 p my_string_reversed
+
+
+#Array stuff
+
+my_array = ["The Walking Dead", "The Simpsons","The Good Place","Cheers","Clueless","Shameless"]
+
+puts my_array.length
+
+puts my_array.first
+puts my_array[0]
+
+puts my_array[3]
+# puts my_array.fourth doesn't work
+
+my_array.reverse!
+
+puts my_array
+p my_array
+my_array.reverse!
+favorite_shows = []
+p favorite_shows
+favorite_shows << my_array[-1]
+favorite_shows << my_array[2]
+
+p favorite_shows
+
+
+
+
+
+
+
