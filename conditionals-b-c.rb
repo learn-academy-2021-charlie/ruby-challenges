d01b72ce816c1e883ae1e6bcf5a18cc84dc64aa2
CSapien08 <sapiano08@gmail.com>
2021-07-28 13:57:05 -0700

    added get.chomp to get it to work

--- conditionals-b-c.rb
+++ conditionals-b-c.rb
@@ -1,28 +1,31 @@
 puts 'enter rock, paper, scissors'
-weapon1 = gets
+weapon1 = gets.chomp
 puts 'enter rock, paper, scissors'
-weapon2 = gets
+weapon2 = gets.chomp
+
 user1 = weapon1
 user2 = weapon2
-puts 'user1 weapon ' +  user1 + 'user2 weapon ' + user2
-if weapon1 == 'rock' && weapon2 == 'rock'
-    puts "tie"
-elsif weapon1 == 'rock' && weapon2 == 'scissors'
+puts 'user1 weapon ' +  weapon1 + 'user2 weapon ' + weapon2
+
+if weapon1 == "rock" and weapon2 == "rock"
+    puts 'tie'
+elsif weapon1 == 'rock' and weapon2 == 'scissors'
     puts "rock wins"
-elsif weapon1 == 'rock' && weapon2 == 'paper'
+elsif weapon1 == 'rock' and weapon2 == 'paper'
     puts 'paper wins'
-elsif weapon1 == 'scissors' && weapon2 == 'rock'
+elsif weapon1 == 'scissors' and weapon2 == 'rock'
     puts 'rock wins'
-elsif weapon1 == 'scissors' && weapon2 == 'paper'
+elsif weapon1 == 'scissors' and weapon2 == 'paper'
     puts 'scissors wins'
-elsif weapon1 == 'scissors' && weapon2 == 'scissors'
+elsif weapon1 == 'scissors' and weapon2 == 'scissors'
     puts 'tie'
-elsif weapon1 == 'paper' && weapon2 == 'rock'
+elsif weapon1 == 'paper' and weapon2 == 'rock'
     puts 'paper wins'
-elsif weapon1 == 'paper' && weapon2 == 'scissors'
+elsif weapon1 == 'paper' and weapon2 == 'scissors'
     puts 'scissors wins'
-elsif weapon1 == 'paper' && weapon2 == 'paper'
+elsif weapon1 == 'paper' and weapon2 == 'paper'
     puts 'tie'
+else
+    puts 'test'
 end
 
-    
