137856b889fee0b3cbc55bae21e11dae57245e7e
Echo5Mike <1Raul.Marquez@gmail.com>
2021-07-28 15:22:15 -0700

    working on excluding

--- ruby-intro.rb
+++ ruby-intro.rb
@@ -2,15 +2,15 @@ my_favorite_number = 7
 # my_favorite_number = my_favorite_number / 2
 # someones_favorite = 13
 # someones_favorite = 7
-p my_favorite_number = my_favorite_number * 26
-p my_favorite_number
+# p my_favorite_number = my_favorite_number * 26
+# p my_favorite_number
 
-my_name = "Thomas"
-p my_name.upcase
+# my_name = "Thomas"
+# p my_name.upcase
 
 # Create a variable and return it in a sentence string interpolation
 
-p "My name is #{my_name}"
+# p "My name is #{my_name}"
 
 # Create a variable that contains a string and test some of the Ruby string methods
 # .upcase
@@ -18,14 +18,14 @@ p "My name is #{my_name}"
 # .include?
 # .capitalize
 
-p my_name.reverse
+# p my_name.reverse
 # mutator with the bang!
-p my_name.upcase!
+# p my_name.upcase!
 
-p my_name
-p my_name.downcase
-p my_name.include?'m'
-p my_name.capitalize
+# p my_name
+# p my_name.downcase
+# p my_name.include?'m'
+# p my_name.capitalize
 
 # p "enter: rock, paper, scissors"
 # selection_user1 = gets.strip
@@ -43,47 +43,74 @@ p my_name.capitalize
 
 # Method Challenge
 
-def add_two_nums(n1, n2)
-    n1 + n2
-end
+# def add_two_nums(n1, n2)
+#     n1 + n2
+# end
 
-p add_two_nums(2, 5)
+# p add_two_nums(2, 5)
 
 
-def is_even(n1)
-    if n1 % 2 == 0
-        "even"
+# def is_even(n1)
+#     if n1 % 2 == 0
+#         "even"
+#     else 
+#         "odd"
+#     end
+# end
+
+# p is_even 5
+
+
+# def is_valid(n1)
+#     if n1 >= 1 && n1 <= 10
+#         "inclusive"
+#     else
+#         "invalid"
+#     end
+# end
+
+# p is_valid 5
+# p is_valid 11
+
+
+# def is_palindrome whatever
+#     if whatever.reverse == whatever
+#         true
+#     else
+#         false
+#     end
+# end
+
+# p is_palindrome "raul"
+# p is_palindrome "racecar"
+
+# Challenge: Password Checker
+# User Stories
+# You are writing the user registration page for a secure web site. On the registration page, the user has to enter a user ID and a password, which has to adhere to the to following criteria:
+
+# As a developer, I can create a method that checks for the following rules for a user ID and password:
+# User ID and password cannot be the same.
+# User ID and password must be at least six characters long.
+# Password must contain at least one of: !#$
+# User ID cannot contain the following characters: !#$ or spaces
+# Password cannot be the word "password".
+
+
+
+def login(id, password)
+arr = [ '!', '#', '$' ]
+    if id != password && id.length >= 6 && password.length >= 6 && arr.any? { |letter| password.include? letter } &&  password != 'password' && id.exclude? '!' && id.exclude? '#' && id.exclude? '$' && id.exclude? ' '
+
+        "good"
     else 
-        "odd"
+        "bad"
     end
 end
+p login("sss!sss", "asdfq!")
 
-p is_even 5
-
-
-def is_valid(n1)
-    if n1 >= 1 && n1 <= 10
-        "inclusive"
-    else
-        "invalid"
-    end
-end
-
-p is_valid 5
-p is_valid 11
-
-
-def is_palindrome whatever
-    if whatever.reverse == whatever
-        true
-    else
-        false
-    end
-end
-
-p is_palindrome "raul"
-p is_palindrome "racecar"
-
-
-
-
+# arr.any? { |letter| id.exclude? letter } 
+# name1= "raul"
+# p name1.include? 'al'
+# arr = [ '!', '#', '$' ]
+# s = "abc12!"
+# p arr.any? { |letter| s.include? letter } # Will return true if any element is present
