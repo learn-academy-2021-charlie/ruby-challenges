7619f400024084122b697e7e2b1be5f9bb0a97b8
lauralyngonzalez <lauralyn.gonzalez@gmail.com>
2021-07-28 15:47:39 -0700

    methods challenges

--- methods-lt.rb
+++ methods-lt.rb
@@ -7,7 +7,7 @@ def is_even num
     num % 2 == 0
 end
 
-p is_even 4
+# p is_even 4
 
 def is_valid num
    if num >= 1 && num <= 10
@@ -17,15 +17,15 @@ def is_valid num
    end
 end
 
-is_valid 1
-is_valid -1
+# is_valid 1
+# is_valid -1
 
 def is_palindrome str
     str.reverse == str
 end
 
-p is_palindrome "racecar"
-p is_palindrome "ToddComte"
+# p is_palindrome "racecar"
+# p is_palindrome "ToddComte"
 
 # uers enter id and password
 # check id not = password
@@ -34,17 +34,25 @@ p is_palindrome "ToddComte"
 # id can not contain any special characters or space
 # password can not be password
 
-def check_login (password, id)
+
+# for indentation, the logical operator and/or must be at the END of the line.
+# we'll get a syntax error if it's at the beginning
+
+def check_login(id, password)
+
+    id_no_spaces = id.delete' ' # delete the spaces in the id for our check
+
     # returns if login is valid
-    if password != id && id.length >= 6 && password.length >= 6
-        && password.include?'!' && password.include?'#' && password.include?'$'
-        && !id.include?'!' && !id.include?'#' && !id.include?'$'
-        && id.length == id.delete' '
-        && password != 'password'
-        true 
+    if password != id and id.length >= 6 and password.length >= 6 and
+        (password.include?'!' or password.include?'#' or password.include?'$') and
+        (!id.include?'!' and !id.include?'#' and !id.include?'$') and
+        id.length == no_spaces.length and password != 'password'
+        true
     else
         false
     end
 end
 
-p check_login ('Tlcomte', 'Tlcomte')
+p check_login('Tlcomte', 'Tlcomte')
+p check_login('id', 'pw!')
+p check_login('hey hello', 'pw')
