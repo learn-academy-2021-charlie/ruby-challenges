aad78ef79a0ab1ca2ae69e127661407a75facdcc
celoazevedo <meazevedo86@gmail.com>
2021-07-28 11:21:04 -0700

    complete  strings and variables challenge

---
+++ ruby-intro.rb
@@ -0,0 +1,30 @@
+my_favorite_number = 7
+# my_favorite_number = my_favorite_number / 2
+# someones_favorite = 13
+# someones_favorite = 7
+p my_favorite_number = my_favorite_number * 26
+p my_favorite_number
+
+my_name = "Thomas"
+p my_name.upcase
+
+# Create a variable and return it in a sentence string interpolation
+
+p "My name is #{my_name}"
+
+# Create a variable that contains a string and test some of the Ruby string methods
+# .upcase
+# .reverse
+# .include?
+# .capitalize
+
+p my_name.reverse
+# mutator with the bang!
+p my_name.upcase!
+
+p my_name
+p my_name.downcase
+p my_name.include?'m'
+p my_name.capitalize
+
+
