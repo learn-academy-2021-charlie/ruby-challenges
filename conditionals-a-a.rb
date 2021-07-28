eb466050a782d1dd06e38897116f498c95f303b4
ArtyG-10 <a.gourent@outlook.com>
2021-07-28 17:08:12 -0400

    added .chomp

--- conditionals-a-a.rb
+++ conditionals-a-a.rb
@@ -7,28 +7,27 @@
 # Story: As a user, I can see a message in the terminal saying if there was a tie.
 
 p "player1 please type either rock, paper, or scissors"
-user1_pick = gets
+user1_pick = gets.chomp
 p "player2 please type either rock, paper, or scissors"
-user2_pick = gets
-
-if (user1_pick == 'rock') && (user2_pick == 'scissors')
-  puts 'user1 wins!'
-elsif (user1_pick == 'scissors') && (user2_pick == 'scissors')
-  puts 'There is a tie!'
-else puts 'this is a test'
+user2_pick = gets.chomp
+if user1_pick == 'rock' && user2_pick == 'scissors'
+    puts 'user1 wins!'
+elsif user1_pick == 'scissors' && user2_pick == 'scissors'
+    puts 'There is a tie!'
+elsif user1_pick == "paper" && user2_pick == "scissors"
+    puts "user2 wins!"
+elsif user1_pick == "rock" && user2_pick == "paper"
+    puts "user2 wins!"
+elsif user1_pick == "rock" && user2_pick == "rock"
+    puts "There is a tie!"
+elsif user1_pick == "scissors" && user2_pick == "rock"
+    puts "user2 wins!"
+elsif user1_pick == "scissors" && user2_pick == "paper"
+    puts "user1 wins!"
+elsif user1_pick == 'paper' && user2_pick == 'paper'
+    puts 'There is a tie!'
+elsif user1_pick == "paper" && user2_pick == "rock"
+    puts 'user1 wins!'
+else
+    puts "something went wrong, please only type either rock, paper, or scissors"
 end
-# else if user1_pick == "paper" && user2_pick == "scissors"
-#   puts "user2 wins!"
-# else if user1_pick == "rock" && user2_pick == "paper"
-#   puts "user2 wins!"
-# else if user1_pick == "rock" && user2_pick == "rock"
-#   puts "There is a tie!"
-# else if user1_pick == "scissors" && user2_pick == "rock"
-#   puts "user2 wins!"
-# else if user1_pick == "scissors" && user2_pick == "paper"
-#   puts "user1 wins!"
-# else if user1_pick == "paper" && user2_pick == "paper"
-#   puts "There is a tie!"
-# else if user1_pick == "paper" && user2_pick == "rock"
-#   puts 'user1 wins!'
-# else puts "something went wrong, please only type either rock, paper, or scissors"
