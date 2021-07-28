a3cc37655d2239e26008b40a994b4c96d76b3133
Jatded1d9l7 <jthoma25wv@gmail.com>
2021-07-28 14:14:50 -0400

    initial commit

---
+++ intro-jared-kelen.rb
@@ -0,0 +1,53 @@
+
+p 1  + 3
+p 6 - 2
+#byebug
+p 2 * 2
+
+p 3 / 2
+
+p 3.5/0.5
+
+p 42 / 2
+
+my_favorite = 42
+
+someones_favorite = 13
+
+someones_favorite = 7
+
+p my_favorite - someones_favorite
+
+my_favorite *= 26
+
+p my_favorite
+
+#strings
+
+goose = 'green goose'
+
+p "My favorite pet is a #{goose}"
+
+p goose.reverse
+
+p goose.upcase
+
+p goose.capitalize
+
+p goose.include?'z'
+
+nickleodean = ['all_that', 'alex_mac', 'legends_of_the_hidden_temple', 'hey_dude', 'are_you_afraid_of_the_dark']
+
+p nickleodean.length
+
+p nickleodean.first + ' and a bag of chips'
+
+p nickleodean[3]
+
+p nickleodean.reverse!
+
+favorites = []
+favorites << nickleodean[2]
+favorites << nickleodean[0]
+
+p favorites
