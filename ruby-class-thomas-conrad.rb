e21b621317b222a43475db68c0646401e12649a9
oddtomas <oddthomas@csu.fullerton.edu>
2021-07-30 11:12:37 -0700

    changing drivers

--- ruby-class-thomas-conrad.rb
+++ ruby-class-thomas-conrad.rb
@@ -48,32 +48,65 @@
 # As a developer, I can initialize each instance of class Task with a status that has a default value of 'incomplete'.
 # As a developer, I can update the status of each instance of class Task when the task has been completed.
 
-class Book
-    def initialize title, page
-        @title = title
-        @page = page
-        @read = false
-    end
-    def get_title
-        @title
-    end
-    def done_read
-        @read = true
-    end
-    def book_info
-        if @read
-            "You have started reading the book #{@title} and you are on page #{@page}."
-        else 
-            "You have not started reading the book #{@title}."
-        end
-    end
+# class Book
+#     def initialize title, page
+#         @title = title
+#         @page = page
+#         @read = false
+#     end
+#     def get_title
+#         @title
+#     end
+#     def done_read
+#         @read = true
+#     end
+#     def book_info
+#         if @read
+#             "You have started reading the book #{@title} and you are on page #{@page}."
+#         else 
+#             "You have not started reading the book #{@title}."
+#         end
+#     end
+# end
+
+# lord_of_the_rings = Book.new 'Lord of the Rings', '50'
+# harry_potter = Book.new 'Harry Potter', '100'
+# twilight = Book.new 'Twilight', '0'
+
+# p lord_of_the_rings.get_title 
+# p lord_of_the_rings.book_info
+# p lord_of_the_rings.done_read
+# p lord_of_the_rings.book_info
+
+
+# As a developer, I can create a class called ColorPalette.
+# As a developer, I can create three instances (objects) of class ColorPalette. (e.g green = ColorPalette.new)
+# As a developer, I can initialize each inistance of the class ColorPalette with three colors. (e.g. green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam'))
+# As a developer, I can print the value of each individual color.
+# As a developer, I can create a method called all_colors that when called will print a sentence telling me the three colors of a given palette.
+# As a developer, I can change one or more colors of a given palette.
+
+class ColorPalette
+  attr_accessor :color1, :color2, :color3
+  def initialize color1, color2, color3
+    @color1 = color1
+    @color2 = color2
+    @color3 = color3
+  end
+
+  def all_colors
+     "#{@color1}, #{@color2}, #{@color3} "
+  end
+
 end
 
-lord_of_the_rings = Book.new 'Lord of the Rings', '50'
-harry_potter = Book.new 'Harry Potter', '100'
-twilight = Book.new 'Twilight', '0'
+green = ColorPalette.new 'Chartreuse', 'Kelly', 'Seafoam'
+red = ColorPalette.new 'magenta', 'burgendy', 'scarlet'
+bleu = ColorPalette.new 'mango', 'citrus', 'blood red'
+# p green.color1
+# p red.color2
+# p bleu.color3
 
-p lord_of_the_rings.get_title 
-p lord_of_the_rings.book_info
-p lord_of_the_rings.done_read
-p lord_of_the_rings.book_info
+p green.all_colors
+green.color1 = "jade"
+p green.all_colors
