b613205ecb9954b8c2b55eb0ce5a61dc933c419c
christophermackey <c.mackey@me.com>
2021-07-30 11:24:49 -0700

    completed challenges

--- objects-trey-chris-alfonso.rb
+++ objects-trey-chris-alfonso.rb
@@ -35,15 +35,25 @@
 # As a developer, I can create three instances (objects) of class ColorPalette. (e.g green = ColorPalette.new)
 # As a developer, I can initialize each inistance of the class ColorPalette with three colors. (e.g. green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam'))
 # As a developer, I can print the value of each individual color.
+
 # As a developer, I can create a method called all_colors that when called will print a sentence telling me the three colors of a given palette.
 # As a developer, I can change one or more colors of a given palette.
 
 class ColorPalette 
-    def initialize color
-        @color = color
+    attr_accessor :color1, :color2, :color3
+    def initialize color1, color2, color3
+        @color1 = color1
+        @color2 = color2
+        @color3 = color3
     end
+    def all_colors
+        p "#{@color1}, #{@color2}, #{@color3}"
+    end    
 end
 
-green = ColorPalette.new 
-magenta = ColorPalette.new
-red = ColorPalette.new
+green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam')
+blue = ColorPalette.new('light blue', 'navy blue', 'dark blue')
+red = ColorPalette.new('crimson', 'mauve', 'firetruck red')
+
+blue.color1 = 'baby blue'
+p blue.color1
