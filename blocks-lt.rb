b81e3c77d6a5a333740426bbf0b322f0a65f81a2
lauralyngonzalez <lauralyn.gonzalez@gmail.com>
2021-07-30 10:02:19 -0700

    last one

--- blocks-lt.rb
+++ blocks-lt.rb
@@ -28,15 +28,34 @@ end
 def evens_backwards
     nums = (0..20).to_a.reverse
     nums.select do |value|
-        p value if value.even?
+        value.even?
     end
 end
 
-evens_backwards
+# step allows us to skip a specified range
+def evens_backwards2
+    (0..20).step(2) do |i|
+        p i
+    end
+end
+
+# evens_backwards2
 
 # Create a method that takes in an array of numbers and returns a new array with all the numbers multiplied by 5.
+def mult5 arr
+    arr.map do |val|
+        val * 5
+    end
+end
+
+p mult5 [1, 2, 3, 4, 5]
+
 
 # Create a method that takes in an array of lowercase words and capitalizes all of the words, permanently modifying the original array.
+def capitalize arr
+    
+end
+
 
 # Create a method that takes in an array of numbers and returns the largest number in the array.
 
@@ -47,6 +66,14 @@ evens_backwards
 # Create a method that takes in an array of strings and returns all the strings in reverse casing. All uppercased letters should be returned lowercased and all lowercased letters should be returned uppercased.
 
 # Create a method that takes in an array of words and returns all the words that start with a particular letter. Ex) Given the array ['Apple', 'Avocado', 'Banana', 'Mango'] and the letter 'a' the method should return ['Apple', 'Avocado']. With the same method, given the letter 'm' should return ['Mango'].
+
+def filterWords(arr, letter)
+    arr.select do |value|
+        value[0].upcase == letter.upcase
+    end
+end
+
+# p filterWords(['Apple', 'Avocado', 'Banana', 'Mango'], 'a')
 
 # FIZZBUZZ
 # Write a method that prints the numbers from 1 to 100. For multiples of three print Fizz instead of the number, for multiples of five print Buzz instead of the number, for numbers that are multiples of both three and five print FizzBuzz, for all other numbers print the number.
