8e73963ccf5de98fe69a328ef70dc6647c0b5118
Andy19936 <andy19936@gmail.com>
2021-07-30 11:24:42 -0700

    complete challenges

--- class-objects-andy-galadoe.rb
+++ class-objects-andy-galadoe.rb
@@ -19,10 +19,26 @@ dinner = Task.new  'food','incomplete'
 
 p dinner.completed
 
-# class ColorPalette
-#   attr_accessor :
-# end
+ class ColorPalette
+   attr_accessor :type1, :type2, :type3
+  def initialize(type1, type2, type3)
+    @type1 = type1
+    @type2 = type2
+    @type3 = type3
+  end
+  def all_colors 
+    "your 3 colors are #{@type1},#{@type2},#{@type3}"
+  end
+  def add_color color
+    @type2 = color
+  end
+ end
 
-# green = ColorPalette.new
-# white = ColorPalette.new
-# purple = ColorPalette.new
+ green = ColorPalette.new('blue','green','orange')
+ white = ColorPalette.new('','','')
+ purple = ColorPalette.new('','','')
+p green.type2
+p green.all_colors
+
+p green.add_color 'blue'
+p green.all_colors
