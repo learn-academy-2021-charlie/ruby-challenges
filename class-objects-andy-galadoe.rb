6c370ff9c8e5ca3045adf77e685fb2882e07928b
Andy19936 <andy19936@gmail.com>
2021-07-30 10:46:20 -0700

    fixed arguments issue

--- class-objects-andy-galadoe.rb
+++ class-objects-andy-galadoe.rb
@@ -1,26 +1,29 @@
 
 class Task
-  def initialize title, status
+  def initialize (title, status)
     @title = title
     @status = 'incomplete'
   end
-  def compeleted 
+  def completed 
     @title = title
     @status = 'completed'
   end
+  def title_info
+    @title
+  end
 end
 
-laundry = Task.new
-grocery = Task.new
-dinner = Task.new 'food', 'completed'
-p dinner.completed
+laundry = Task.new 'clothes', 'incomplete'
+grocery = Task.new 'food', 'incomplete'
+dinner = Task.new  'completed'
+ dinner.title_info 
 
-# p dinner.completed
+p dinner.completed
 
 # class ColorPalette
 #   attr_accessor :
 # end
-#
+
 # green = ColorPalette.new
 # white = ColorPalette.new
 # purple = ColorPalette.new
