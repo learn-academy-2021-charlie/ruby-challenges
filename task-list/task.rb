e20d07d124493192ad0fc7ce757907a59afb2e9b
hroulston <h_roulston@live.com>
2021-08-03 13:34:19 -0700

    completed until task status done

--- task-list/task.rb
+++ task-list/task.rb
@@ -1,19 +1,35 @@
 # Story: As a developer, I can create a Task.
+# Story: As a developer, I can give a Task a title and retrieve it.
+# Story: As a developer, I can give a Task a description and retrieve it.
+# Story: As a developer, I can mark a Task done. Tasks should be initialized as 'in progress'.
+# Story: As a developer, when I print a Task that is done, its status is shown.
+
+
+
+
 class Task
+  def initialize status='in progress'
+    @status=status
+  end
+  def status
+    @status
+  end
     def wash=wash_dishes
         @wash=wash_dishes
     end
     def wash
         @wash
     end
+    def description=task_describe
+      @description=task_describe
+    end
+    def description
+      @description
+    end
 end
-# Story: As a developer, I can give a Task a title and retrieve it.
 
-# Story: As a developer, I can give a Task a description and retrieve it.
 
-# Story: As a developer, I can mark a Task done. Tasks should be initialized as 'in progress'.
 
-# Story: As a developer, when I print a Task that is done, its status is shown.
 
 # Story: As a developer, I can add all of my Tasks to a TaskList.
 
--- task-list/task_spec.rb
+++ task-list/task_spec.rb
@@ -12,4 +12,17 @@ describe 'Task' do
         expect(my_task.wash).to be_a String
         expect(my_task.wash).to eq 'Wash Dishes'
     end
+    it 'describes the task' do
+      my_task = Task.new
+      my_task.description = 'All of the dishes in the sink get washed.'
+      expect(my_task.description).to be_a String
+      expect(my_task.description).to eq 'All of the dishes in the sink get washed.'
+    end
+    it 'marks task as done, will be in progress as default' do
+      my_task = Task.new
+      expect(my_task.status).to be_a String
+      expect(my_task.status).to eq 'in progress'
+      my_task = Task.new 'Done'
+      expect(my_task.status).to eq 'Done'
+    end
 end
