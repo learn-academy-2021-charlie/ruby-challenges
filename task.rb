a3c627fad6648dcb5d71b90960cc1d4cd3fc38dd
ArtyG-10 <a.gourent@outlook.com>
2021-08-03 14:54:35 -0400

    driver switch

---
+++ task.rb
@@ -0,0 +1,11 @@
+class Task
+  attr_accessor :title, :description, :status
+  def initialize title, description
+  @title = title
+  @description = description
+  @status = 'in progress'
+end
+  def finish
+    @status = 'done'
+  end
+end
