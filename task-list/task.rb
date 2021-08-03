5ec43a1578e9e0565f10cf4486c1cd49aae9c660
Chrisluna10 <chrisluna4224@gmail.com>
2021-08-03 14:06:45 -0700

    two challenges left

--- task-list/task.rb
+++ task-list/task.rb
@@ -3,6 +3,7 @@
 # Story: As a developer, I can give a Task a description and retrieve it.
 # Story: As a developer, I can mark a Task done. Tasks should be initialized as 'in progress'.
 # Story: As a developer, when I print a Task that is done, its status is shown.
+# Story: As a developer, I can add all of my Tasks to a TaskList.
 
 
 
@@ -29,9 +30,6 @@ class Task
 end
 
 
-
-
-# Story: As a developer, I can add all of my Tasks to a TaskList.
 
 # Story: As a developer with a TaskList, I can print the completed items.
 
